9a1f7c0c977bdd7993286cd82ec0f96e081037a2
Mirshahi <mirshahi@utb.cz>
2020-05-22 08:43:06 +0200

    new update

--- whyfi.py
+++ whyfi.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 
 
 from multiprocessing import Process
@@ -9,26 +6,27 @@ import os
 from randmac import RandMac
 import random
 import string
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--paranoid", nargs="?", default=True, help="It defines if your data goes over a VPN or not, the defualt value is True")
+args = parser.parse_args()
+paranoid = args.paranoid
 
 #VARIABLES TO SET THESE SHOULD BE SET ACORDING TO YOURS
 #FIND THEM USING sudo ip link show and set them here
 
 lan = "enp2s0"
 wireless = "wlp3s0"
-
-
-
-ap = "ap0" #usually you do not need to change it
-time_shift = 2 #this is the time for shifting the packets from the conected device to your hotspot to confuse ISP 
-
+ap = "ap0" #usually you do not how to change it
+time_shift = 2 #this is the time for shifting the packets from the conected device to your hotspot to confuse ISP
 def randomString(stringLength=10):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
 def hotspot():
-    channels = [3,4,5,8,7,9,10,12,13]#list of channel from which a channel will be used to broadcast
+    channels = [3,4,5,8,7,9,10]#list of channel from which a channel will be used to broadcast
     random_mac = RandMac("00:00:00:00:00:00", True)
     line = "create_ap -c "+str(channels[random.randint(0, len(channels)-1 )])+" --hidden --mac "+str(random_mac)+" "+wireless+" "+lan+" "+randomString(random.randint(1,4))+" "+randomString(8)
     print(line)
@@ -38,11 +36,13 @@ def ttl_shift():
     print("Ditch out the ISP for mac address filtering ^_^...")
     os.system("iptables -w -t mangle -I PREROUTING -i "+ap+" -j TTL --ttl-inc "+str(time_shift))
     os.system("iptables -w -t mangle -I PREROUTING -i "+lan+" -j TTL --ttl-inc "+str(time_shift))
-    print("Now you can connect! , press Ctrl+C to disconnect")
+    print("Catch me if you can, I am in dude wating for your command sir")
 
 
 def cleanup():
     print("Doing clean up...")
+    os.system("sudo ip link set ap0 down")
+    os.system("sudo iw dev ap0 del")
     for file in os.listdir("/tmp"):
         if file.endswith(".lock"):
             case = (os.path.join("/tmp", file))
@@ -52,15 +52,39 @@ def cleanup():
     print("done with clean up")
 
 
-print("HOT SPOT IS STARTING")
+def vpn(): #Here I am using ProtonVpn in CLI mode, you already had to install it and configure it with your own account
+    print("Rounting the data over VPN...")
+    os.system("protonvpn c -f")
+    os.system("protonvpn s")
+
+
+
+def fun():
+    os.system('figlet HOTSPOT IS COMMING')
+
+
+
+def wifi_down():
+    os.system('nmcli con down eduroam')
+
+
+
+fun()
+cleanup()
+wifi_down()
 p1 = Process(target=hotspot)
 p2 = Process(target=ttl_shift)
+p3 = Process(target=vpn)
 
-cleanup()
+
+if paranoid == True:
+	p3.start()
+	sleep(3)
 p1.start()
 print("Enabling Hotspot")
-sleep(7)
+sleep(7) # So the ap being created and then after we do the time shift
 p2.start()
+
 
 
 
